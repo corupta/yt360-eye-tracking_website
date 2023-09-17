1c6683fadba3b42430cf0fa3a2a49fac7a0d5765
corupta <halitozsoy1584@gmail.com>
2023-09-18 01:03:12 +0300

    rename chosen to curated in home page header

--- src/Header/TopHeader.js
+++ src/Header/TopHeader.js
@@ -46,18 +46,18 @@ export default class TopHeader extends Component {
         <h1 style={titleStyle}>
           <img 
             src="/assets/logos/yt-360-curated-eye.png" 
-            alt="YT-360-Chosen-Eye Dataset" 
-            title="YT-360-Chosen-Eye Dataset"
+            alt="YT-360-Curated-Eye Dataset" 
+            title="YT-360-Curated-Eye Dataset"
             style={logoStyle} 
           />
-          YT-360-Chosen-Eye Dataset
+          YT-360-Curated-Eye Dataset
         </h1>
         <h3 style={subtitleStyle}>
           A curated dataset of 360 degree video content with spatial audio, alongside eye-tracking data from observers of different conditions.
         </h3>
         <img 
-          alt="YT-360-Chosen-Eye Dataset"
-          title="YT-360-Chosen-Eye Dataset"
+          alt="YT-360-Curated-Eye Dataset"
+          title="YT-360-Curated-Eye Dataset"
           src="/assets/collages/thumbnails_target_rows__en.png" 
           style={imgStyle} 
         />
